3d0a08220ba0c548fb0607b85260ca9bffcf5ec2
gongyihui <gyh1429107530@gmail.com>
2022-02-28 17:28:23 +0000

    it is use to test Data

---
+++ src/test/java/uk/comp2211/group13/DataTest.java
@@ -0,0 +1,23 @@
+package uk.comp2211.group13;
+
+import org.junit.Test;
+
+public class DataTest {
+    @Test
+    public void ingestTest(){
+
+    }
+    @Test
+    public void validatePathTest(){
+
+    }
+    @Test
+    public void validateLogFormatTest(){
+
+    }
+    @Test
+    public void requestTest(){
+
+    }
+}
+
