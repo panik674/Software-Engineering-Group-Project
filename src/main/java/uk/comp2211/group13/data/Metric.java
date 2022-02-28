3854ad3954eb21722e868d221747ca1b8906958b
Thomas Cross <thomas@thomascross.net>
2022-02-28 15:20:47 +0000

    Add Path and Metric enums

---
+++ src/main/java/uk/comp2211/group13/data/Metric.java
@@ -0,0 +1,18 @@
+package uk.comp2211.group13.data;
+
+/**
+ * This stores the metrics which we can request from the Metrics class
+ */
+public enum Metric {
+  Impressions,
+  Clicks,
+  Unique,
+  Bounces,
+  Conversions,
+  TotalCost,
+  CTR,
+  CPA,
+  CPC,
+  CPM,
+  BounceRate
+}
---
+++ src/main/java/uk/comp2211/group13/data/Path.java
@@ -0,0 +1,10 @@
+package uk.comp2211.group13.data;
+
+/**
+ * This enum stores the types of log files the Data class will accept.
+ */
+public enum Path {
+  Impression,
+  Click,
+  Server
+}
