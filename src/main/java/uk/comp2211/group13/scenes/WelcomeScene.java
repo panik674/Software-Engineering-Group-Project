763594f76ab0b9402348f35093529a06e78b4377
Yaseen Mohammed Osman <yaseenkamaltag@gmail.com>
2022-03-02 12:14:39 +0000

    Created the base structure of the welcome screen scene.

---
+++ src/main/java/uk/comp2211/group13/scenes/WelcomeScene.java
@@ -0,0 +1,31 @@
+package uk.comp2211.group13.scenes;
+
+import uk.comp2211.group13.ui.AppWindow;
+
+public class WelcomeScene extends BaseScene {
+
+    /**
+     * Creates a new scene.
+     *
+     * @param appWindow the app window that displays the scene
+     */
+    public WelcomeScene(AppWindow appWindow) {
+        super(appWindow);
+    }
+
+    /**
+     * Initialise the scene and start the game
+     */
+    @Override
+    public void initialise() {
+
+    }
+
+    /**
+     * Build the Welcome window
+     */
+    @Override
+    public void build() {
+
+    }
+}
