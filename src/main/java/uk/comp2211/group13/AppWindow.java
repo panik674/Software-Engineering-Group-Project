9617c775f680a6539171079d9dbc0c85ce993019
Yaseen Mohammed Osman <yaseenkamaltag@gmail.com>
2022-03-02 12:00:52 +0000

    Added comments/description to AppWindow class.

--- src/main/java/uk/comp2211/group13/AppWindow.java
+++ src/main/java/uk/comp2211/group13/AppWindow.java
@@ -13,15 +13,10 @@ import uk.comp2211.group13.scenes.BaseScene;
 
 // The code here has been taken from our last year Programming II Coursework
 /**
- * The App Pane is a special pane which will scale anything inside it to the screen and maintain the aspect ratio.
+ * The AppWindow is the single window for the app where everything takes place. To move between screens in the game,
+ * we simply change the scene.
  *
- * Drawing will be scaled appropriately.
- *
- * This takes the worry about the layout out and will allow the app to scale to any resolution easily.
- *
- * It uses the width and height given which should match the main window size. This will be the base drawing resolution,
- * but will be scaled up or down as the window is resized.
- *
+ * The AppWindow has methods to launch each of the different parts of the app by switching scenes.
  */
 
 public class AppWindow {
