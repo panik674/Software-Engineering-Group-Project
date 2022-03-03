b43fd3d0a229c4560045ca8a06125b6c82fdd3e5
Thomas Cross <thomas@thomascross.net>
2022-03-03 17:49:53 +0000

    Add java doc to Click and Server records

--- src/main/java/uk/comp2211/group13/data/log/Click.java
+++ src/main/java/uk/comp2211/group13/data/log/Click.java
@@ -1,3 +1,6 @@
 package uk.comp2211.group13.data.log;
 
+/**
+ * This record is used to store a row of the click log.
+ */
 public record Click(String date, String id, Float cost) { }
--- src/main/java/uk/comp2211/group13/data/log/Server.java
+++ src/main/java/uk/comp2211/group13/data/log/Server.java
@@ -1,3 +1,6 @@
 package uk.comp2211.group13.data.log;
 
+/**
+ * This record is used to store a row of the server log.
+ */
 public record Server(String entryDate, String id, String exitDate, Integer pages, Boolean conversion) { }
