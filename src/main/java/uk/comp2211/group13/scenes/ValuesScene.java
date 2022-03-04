4e3a57f7d824140fb6146a66e8c00807d2518787
Yaseen Mohammed Osman <yaseenkamaltag@gmail.com>
2022-03-04 15:37:39 +0000

    Added one ui component and the feature of exiting to start screen with ESC.

--- src/main/java/uk/comp2211/group13/scenes/ValuesScene.java
+++ src/main/java/uk/comp2211/group13/scenes/ValuesScene.java
@@ -1,13 +1,19 @@
 package uk.comp2211.group13.scenes;
 
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import uk.comp2211.group13.ui.AppPane;
 import uk.comp2211.group13.ui.AppWindow;
 
 public class ValuesScene extends BaseScene {
 
     private static final Logger logger = LogManager.getLogger(ValuesScene.class);
 
+    private StackPane valuesPane;
     /**
      * Creates a new scene.
      *
@@ -27,15 +33,32 @@ public class ValuesScene extends BaseScene {
     }
 
     /**
-     * Build the Welcome window
+     * Build the Values window
      */
     @Override
     public void build() {
+        logger.info("Building " + this.getClass().getName());
 
+        root = new AppPane(appWindow.getWidth(),appWindow.getHeight());
+
+        valuesPane = new StackPane();
+        valuesPane.setMaxWidth(appWindow.getWidth());
+        valuesPane.setMaxHeight(appWindow.getHeight());
+
+        root.getChildren().add(valuesPane);
+
+        var mainPane = new BorderPane();
+        valuesPane.getChildren().add(mainPane);
+
+        Text text = new Text("Values goes here");
+        mainPane.setCenter(text);
     }
 
     @Override
     public void events() {
-
+        scene.setOnKeyPressed((e) -> {
+            if (e.getCode() != KeyCode.ESCAPE) return;
+            appWindow.welcomeScreen();
+        });
     }
 }
