7d9382cfca80ed11771ef42c096851b4144515bd
Kiore <83663533+gong2002@users.noreply.github.com>
2022-03-04 17:40:03 +0000

    Update DataTest.java

--- src/test/java/uk/comp2211/group13/DataTest.java
+++ src/test/java/uk/comp2211/group13/DataTest.java
@@ -1,94 +1,72 @@
-package uk.comp2211.group13;
+ackage uk.comp2211.group13;
 
 import org.junit.Assert;
 import org.junit.Test;
 import uk.comp2211.group13.data.Data;
-import uk.comp2211.group13.data.Logs;
+import uk.comp2211.group13.data.Metrics;
 import uk.comp2211.group13.enums.Path;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
 
-public class DataTest {
-    private Data data = new Data();
-    private Logs logs = new Logs();
-
+public class MetricsTest {
+    Data data = new Data();
+    Metrics metrics = new Metrics(data);
     public void ingest(){
         HashMap<Path, String> pathsTest = new HashMap<>();
-        pathsTest.put(Path.Click,"src/test/java/resources/2_week_campaign_2/click_log.csv");
-        pathsTest.put(Path.Impression,"src/test/java/resources/2_week_campaign_2/impression_log.csv");
-        pathsTest.put(Path.Server,"src/test/java/resources/2_week_campaign_2/server_log.csv");
+        pathsTest.put(Path.Click,"src/test/java/uk/comp2211/group13/click_log.csv");
+        pathsTest.put(Path.Impression,"src/test/java/uk/comp2211/group13/impression_log.csv");
+        pathsTest.put(Path.Server,"src/test/java/uk/comp2211/group13/server_log.csv");
         data.ingest(pathsTest);
     }
     @Test
-    public void ingestTest(){
-        HashMap<Path, String> pathsTest = new HashMap<>();
-        pathsTest.put(Path.Click,"src/test/java/resources/2_week_campaign_2/click_log.csv");
-        pathsTest.put(Path.Impression,"src/test/java/resources/2_week_campaign_2/impression_log.csv");
-        pathsTest.put(Path.Server,"src/test/java/resources/2_week_campaign_2/server_log.csv");
-        Assert.assertSame("test ingest",true,data.ingest(pathsTest));
-        Assert.assertNotNull(logs.serverLogs);
-        Assert.assertNotNull(logs.clickLogs);
-        Assert.assertNotNull(logs.impressionLogs);
+    public void clickRateTest(){
+        ingest();
+        double result = 20;
+        Assert.assertNotNull(metrics.clickRate());
+        Assert.assertEquals(BigDecimal.valueOf(result),BigDecimal.valueOf(metrics.clickRate()));
     }
     @Test
-    public void requestTest(){
+    public void bounceRateTest(){
         ingest();
-        Assert.assertNotNull(data.request());
-        Assert.assertNotNull(data.request().clickLogs);
-        Assert.assertNotNull(data.request().impressionLogs);
-        Assert.assertNotNull(data.request().serverLogs);
+        double result=0;
+        Assert.assertNotNull(metrics.bounceRate());
+        Assert.assertEquals(BigDecimal.valueOf(result),BigDecimal.valueOf(metrics.bounceRate()));
     }
     @Test
-    public void getClicksTest(){
+    public void conversionRateTest(){
         ingest();
-        int resultFor2_week = 23923;
-        Assert.assertNotNull(data.getClicks());
-        Assert.assertEquals(resultFor2_week,data.getClicks());
+        double result = 0;
+        Assert.assertNotNull(metrics.conversionRate());
+        Assert.assertEquals(BigDecimal.valueOf(result),BigDecimal.valueOf(metrics.conversionRate()));
     }
     @Test
-    public void getImpressionTest(){
+    public void costThousandTest(){
         ingest();
-        int resultFor2_week = 486104;
-        Assert.assertNotNull(data.getImpressions());
-        Assert.assertEquals(resultFor2_week,data.getImpressions());
+        double result = 1.0019391775131226;
+        Assert.assertNotNull(metrics.costThousand());
+        Assert.assertEquals(BigDecimal.valueOf(result),BigDecimal.valueOf(metrics.costThousand()));
     }
     @Test
-    public void getBounceTest(){
+    public void costAcquisitionTest(){
         ingest();
-        int resultFor2_week = 7826;
-        Assert.assertNotNull(data.getBounces());
-        Assert.assertEquals(resultFor2_week,data.getBounces());
-
+        double result = 58.29112243652344;
+        Assert.assertNotNull(metrics.costAcquisition());
+        Assert.assertEquals(BigDecimal.valueOf(result),BigDecimal.valueOf(metrics.costAcquisition()));
     }
     @Test
-    public void getConversionsTest(){
+    public void costClickTest(){
         ingest();
-        int resultFor2_week = 2026;
-        Assert.assertNotNull(data.getConversions());
-        Assert.assertEquals(resultFor2_week,data.getConversions());
+        double result = 4.916221618652344;
+        Assert.assertNotNull(metrics.costClick());
+        Assert.assertEquals(BigDecimal.valueOf(result),BigDecimal.valueOf(metrics.costClick()));
     }
     @Test
-    public void getUniquesTest(){
+    public void totalCastTest(){
         ingest();
-        int resultFor2_week = 23806;
-        Assert.assertNotNull(data.getUniques());
-        Assert.assertEquals( resultFor2_week,data.getUniques());
+        double result = 118097.8125;
+        Assert.assertNotNull(data.getImpressionCost()+data.getClickCost());
+        Assert.assertEquals(BigDecimal.valueOf(result),BigDecimal.valueOf(data.getImpressionCost()+data.getClickCost()));
     }
-    @Test
-    public void getClickCost(){
-        ingest();
-        double resultFor2_week = 117610.765625;
-        Assert.assertNotNull(data.getClickCost());
-       Assert.assertEquals(BigDecimal.valueOf(resultFor2_week),BigDecimal.valueOf(data.getClickCost()));
-    }
-    @Test
-    public void getImpressionCost(){
-        ingest();
-        double resultFor2_week =487.0466613769531;
-        Assert.assertNotNull(data.getImpressionCost());
-        Assert.assertEquals(BigDecimal.valueOf(resultFor2_week),BigDecimal.valueOf(data.getImpressionCost()));
-    }
-
 }
 
