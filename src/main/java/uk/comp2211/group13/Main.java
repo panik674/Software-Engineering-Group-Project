4854bbaa4c58fc67c302d18ea8aecf14870b95f6
Thomas Cross <thomas@thomascross.net>
2022-03-08 22:51:03 +0000

    Add launcher and shader to POM

---
+++ src/main/java/uk/comp2211/group13/Main.java
@@ -0,0 +1,11 @@
+package uk.comp2211.group13;
+
+/**
+ * This is used to launch the Application
+ * This explains why: https://stackoverflow.com/questions/52653836/maven-shade-javafx-runtime-components-are-missing
+ */
+public class Main {
+  public static void main(String[] args) {
+    App.main(args);
+  }
+}
