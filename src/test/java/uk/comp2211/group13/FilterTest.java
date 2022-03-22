89e8ce956c2ed94990fa923186faf46d3c43dfc6
Kiore <83663533+Hui-cd@users.noreply.github.com>
2022-03-22 17:18:56 +0000

    Update FilterTest.java

--- src/test/java/uk/comp2211/group13/FilterTest.java
+++ src/test/java/uk/comp2211/group13/FilterTest.java
@@ -4,7 +4,9 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import uk.comp2211.group13.data.Data;
 import uk.comp2211.group13.enums.Filter;
+import java.text.ParseException;
 import java.util.*;
 
 public class FilterTest {
@@ -13,10 +15,19 @@ public class FilterTest {
     private String[] gender;
     private String[] income;
     private String[] context;
+    private Data data;
+
 
 
     @Before
     public void setupData() {
+        data = new Data();
+
+        ArrayList<String> pathsTest = new ArrayList<>();
+        pathsTest.add("src/test/java/uk/comp2211/group13/testdata/click_log.csv");
+        pathsTest.add("src/test/java/uk/comp2211/group13/testdata/impression_log.csv");
+        pathsTest.add("src/test/java/uk/comp2211/group13/testdata/server_log.csv");
+        data.ingest(pathsTest);
         age = new String[]{"<25", "25-34", "35-44", "45-54", ">54"};
         gender = new String[] {"Male", "Female"};
         income = new String[]{"Low", "Medium", "High"};
@@ -46,6 +57,17 @@ public class FilterTest {
         Assert.assertTrue(filterMap.get(Filter.Age).length == size);
         Assert.assertArrayEquals(result,filterMap.get(Filter.Age));
     }
+    @Test
+    public void ageFilterFunctionTest() throws ParseException {
+        filterMap.clear();
+        String[] age = {"<25", "25-34", "35-44", "45-54", ">54"};
+        filterMap.put(Filter.Age,age);
+        int result = 23923;
+        Assert.assertEquals(result,data.request(Utility.string2Date("2015-01-01 12:00:00"),
+                Utility.string2Date("2015-01-15 12:00:00"),
+                filterMap
+        ).getClicks());
+    }
 
     @Test
     public void genderFilterTest(){
@@ -58,6 +80,17 @@ public class FilterTest {
         Assert.assertTrue(filterMap.get(Filter.Gender).length!=result.length);
     }
     @Test
+    public void genderFilterFunctionTest() throws ParseException {
+        filterMap.clear();
+        String[] gender = {"Female"};
+        filterMap.put(Filter.Gender,gender);
+        int result = 15935;
+        Assert.assertEquals(result,data.request(Utility.string2Date("2015-01-01 12:00:00"),
+                Utility.string2Date("2015-01-15 12:00:00"),
+                filterMap
+        ).getClicks());
+    }
+    @Test
     public void incomeFilterTest(){
         String[] result = {"Low", "Medium", "High"};
         Assert.assertArrayEquals(result,filterMap.get(Filter.Income));
@@ -68,6 +101,17 @@ public class FilterTest {
         Assert.assertTrue(filterMap.get(Filter.Income).length!=result.length);
     }
     @Test
+    public void incomeFilterFunctionTest() throws ParseException {
+        filterMap.clear();
+        String[] income =  { "Medium", "High"};
+        filterMap.put(Filter.Income,income);
+        int result = 19345;
+        Assert.assertEquals(result,data.request(Utility.string2Date("2015-01-01 12:00:00"),
+                Utility.string2Date("2015-01-15 12:00:00"),
+                filterMap
+        ).getClicks());
+    }
+    @Test
     public void contextFilterTest(){
         String[] result = {"News", "Shopping", "Social Media", "Blog", "Hobbies", "Travel"};
         Assert.assertArrayEquals(result,filterMap.get(Filter.Context));
@@ -76,6 +120,17 @@ public class FilterTest {
     public void contextLackTest(){
         String[] result = {"News", "Shopping"};
         Assert.assertTrue(filterMap.get(Filter.Context).length!= result.length);
+    }
+    @Test
+    public void contextFilterFunctionTest() throws ParseException {
+        filterMap.clear();
+        String[] context =  {"News", "Travel"};
+        filterMap.put(Filter.Context,context);
+        int result = 6716;
+        Assert.assertEquals(result,data.request(Utility.string2Date("2015-01-01 12:00:00"),
+                Utility.string2Date("2015-01-15 12:00:00"),
+                filterMap
+        ).getClicks());
     }
 }
 
