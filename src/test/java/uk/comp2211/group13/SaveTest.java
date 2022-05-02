ada0d7f4489596cea6f7be83701542cc829259b6
Kiore <83663533+Hui-cd@users.noreply.github.com>
2022-05-02 17:30:28 +0100

    add save test

--- src/test/java/uk/comp2211/group13/SaveTest.java
+++ src/test/java/uk/comp2211/group13/SaveTest.java
@@ -11,7 +11,9 @@ import uk.comp2211.group13.enums.Metric;
 
 import java.io.*;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 
 import static uk.comp2211.group13.data.Save.getDirectory;
@@ -20,41 +22,61 @@ import static uk.comp2211.group13.data.Save.getDirectory;
 public class SaveTest  {
     private Data data;
     private Save save;
-    private HashSet<Filter> filters;
-    private HashSet<Metric> metrics;
-    private Date startDate;
-    private Date endDate;
-    private Granularity granularity;
+    public  HashMap<Filter, String[]> genderFilters;
+    public  HashMap<Filter, String[]> ageFilters;
+    public  HashMap<Filter, String[]> incomeFilters;
+    public  HashMap<Filter, String[]> contextFilters;
+    public  Date startDate;
+    public  Date endDate;
+    public  Granularity granularity;
     @Before
     public void setupData() throws ParseException {
         save = new Save();
-        filters =new HashSet<>();
-        metrics = new HashSet<>();
+        genderFilters = new HashMap<>();
+        ageFilters = new HashMap<>();
+        incomeFilters = new HashMap<>();
+        contextFilters = new HashMap<>();
         startDate = new Date();
         endDate = new Date();
 
-        filters.add(Filter.Gender);
-        metrics.add(Metric.Impressions);
+        genderFilters.put(Filter.Gender,new String[]{"Male", "Female"});
+        ageFilters.put(Filter.Age,new String[]{"<25", "25-34", "35-44", "45-54", ">54"});
+        incomeFilters.put(Filter.Income,new String[]{"Low", "Medium", "High"});
+        contextFilters.put(Filter.Context,new String[]{"News", "Shopping", "Social Media", "Blog", "Hobbies", "Travel"});
         endDate = Utility.string2Date("2015-01-15 12:00:00");
         startDate = Utility.string2Date("2015-01-01 12:00:00");
-        save.saveFilters(filters,metrics,Granularity.Second,startDate,endDate);
+
+        save.saveFilters(genderFilters,ageFilters,incomeFilters,contextFilters,startDate,endDate,Granularity.Second);
         save.loadFilters();
     }
     /**This test will test save filters
      * */
     @Test
-    public void testSaveFilters(){
+    public void testSaveFilters() throws FileNotFoundException {
         String path = getDirectory();
-        File file = new File(path);
-        Assert.assertNotNull(file);
+        FileReader genderFile = new FileReader(path+ "gender.json");
+        FileReader ageFile = new FileReader(path +  "age.json");
+        FileReader incomeFile = new FileReader(path + "income.json");
+        FileReader contextFile = new FileReader(path +"context.json" );
+        FileReader dateFile = new FileReader(path + "date.json");
+        FileReader granularityFile = new FileReader(path + "granularity.json");
+        Assert.assertNotNull(genderFile);
+        Assert.assertNotNull(ageFile);
+        Assert.assertNotNull(incomeFile);
+        Assert.assertNotNull(contextFile);
+        Assert.assertNotNull(dateFile);
+        Assert.assertNotNull(granularityFile);
     }
     /**This test will test load filter**/
     @Test
     public void testLoadFilters() {
-        Assert.assertNotNull(save.filters);
-        Assert.assertNotNull(save.metrics);
-        Assert.assertNotNull(save.granularity);
-        Assert.assertNotNull(save.startDate);
-        Assert.assertNotNull(save.endDate);
+        Assert.assertNotNull(Save.incomeFilters);
+        Assert.assertNotNull(Save.ageFilters);
+        Assert.assertNotNull(Save.genderFilters);
+        Assert.assertNotNull(Save.contextFilters);
+        Assert.assertNotNull(Save.endDate);
+        Assert.assertNotNull(Save.startDate);
+        Assert.assertNotNull(Save.granularity);
+
     }
 }
